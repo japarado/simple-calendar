9db4f88bdcc7efda56d4305095e49336c5ec8215
pam <justin.parado.personal@gmail.com>
2020-12-17 22:55:54 +0800

    Event and event date migrations

---
+++ database/migrations/2020_12_17_094602_create_event_date_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventDateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_date', function (Blueprint $table) {
+            $table->id();
+			$table->date('date');
+			$table->unsignedBigInteger('event_id');
+            $table->timestamps();
+			$table->softDeletes();
+
+			$table->foreign('event_id')->references('id')->on('event');
+
+			$table->index('date');
+			$table->index('event_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_date');
+    }
+}
