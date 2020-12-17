d0c9b4c050ceb0738130e96eea25885aa34ec2fa
pam <justin.parado.personal@gmail.com>
2020-12-17 16:44:37 +0800

    Event day checkbox disabling of unavailable days
    Available days depend on the date range

--- resources/js/components/CalendarForm/CalendarForm.js
+++ resources/js/components/CalendarForm/CalendarForm.js
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-import {differenceInDays} from "date-fns";
+import {differenceInCalendarDays, eachDayOfInterval} from "date-fns";
 
 import {createDayArray} from "../../utils";
 
@@ -15,16 +15,32 @@ const CalendarForm = (props) =>
 	const [endDate, setEndDate] = useState(new Date());
 	const [days, setDays] = useState(createDayArray());
 
+	useEffect(() => 
+	{
+		const eventDays = eachDayOfInterval({start: startDate, end: endDate}).map((day) => day.getDay());
+		const availableDays = days.map((day) => 
+		{
+			if(eventDays.includes(day.value))
+			{
+				day.isAvailable = true;
+			}
+			else 
+			{
+				day.isAvailable = day.checked = false;
+			}
+			return day;
+		});
+			setDays(availableDays);
+	}, [startDate, endDate]);
+
 	const handleUpdateStartDate = (date) => 
 	{
 		setStartDate(date);
-		console.log(differenceInDays(startDate, endDate));
 	};
 
 	const handleUpdateEndDate = (date) => 
 	{
 		setEndDate(date);
-		console.log(differenceInDays(startDate, endDate));
 	};
 
 	const handleCheckDay = (e) => 
@@ -47,86 +63,90 @@ const CalendarForm = (props) =>
 
 	return (
 		<>
-			<div className="form-row">
-				<div className="form-group col-md-12">
-					<label htmlFor="event-name">Event Name</label>
-					<input
-						type="text"
-						className="form-control"
-						id="event-name"
-						name="event-name"
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-						required/>
-				</div>
-			</div>
-
-			<div className="form-row">
-				<div className="form-group col-md-12">
-					<label htmlFor="description">Description</label>
-					<textarea
-						className="form-control"
-						id="description"
-						name="description"
-						value={description}
-						onChange={(e) => setDescription(e.target.value)}
-					/>
-				</div>
-			</div>
-
-			<div className="form-row">
-				<div className="form-group col-md-6">
-					<div className="d-flex flex-column">
-						<label htmlFor="start-date">Start Date</label>
-						<DatePicker
-							selected={startDate}
-							onChange={(date) => handleUpdateStartDate(date)}
-							dateFormat="dd MMMM yyyy"
-							maxDate={endDate}
+			<form onSubmit={props.handleSubmitCreate}>
+				<div className="form-row">
+					<div className="form-group col-md-12">
+						<label htmlFor="event-name">Event Name</label>
+						<input
+							type="text"
 							className="form-control"
-							id="start-date"
+							id="event-name"
+							name="event-name"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+							required/>
+					</div>
+				</div>
+
+				<div className="form-row">
+					<div className="form-group col-md-12">
+						<label htmlFor="description">Description</label>
+						<textarea
+							className="form-control"
+							id="description"
+							name="description"
+							value={description}
+							onChange={(e) => setDescription(e.target.value)}
 						/>
 					</div>
 				</div>
 
-				<div className="form-group col-md-6">
-					<div className="d-flex flex-column">
-						<label htmlFor="start-date">End Date</label>
-						<DatePicker
-							selected={endDate}
-							onChange={(date) => handleUpdateEndDate(date)}
-							dateFormat="dd MMMM yyyy"
-							minDate={startDate}
-							className="form-control"
-							id="start-date"
-						/>
-					</div>
-				</div>
-
-				<div className="form-group">
-					{days.map((day) => (
-						<div
-							className="form-check form-check-inline"
-							key={day.label}>
-							<input
-								className="form-check-input"
-								type="checkbox"
-								id={day.label}
-								name={day.label}
-								value={day.value}
-								onClick={props.handleUpdateDay}
-								disabled={!day.isAvailable}
-								onChange={handleCheckDay}
-								checked={day.checked && day.isAvailable}
+				<div className="form-row">
+					<div className="form-group col-md-6">
+						<div className="d-flex flex-column">
+							<label htmlFor="start-date">Start Date</label>
+							<DatePicker
+								selected={startDate}
+								onChange={(date) => handleUpdateStartDate(date)}
+								dateFormat="dd MMMM yyyy"
+								maxDate={endDate}
+								className="form-control"
+								id="start-date"
 							/>
-							<label
-								className="form-check-label"
-								htmlFor="sunday">{day.label}</label>
 						</div>
-					))}
-				</div>
+					</div>
 
-			</div>
+					<div className="form-group col-md-6">
+						<div className="d-flex flex-column">
+							<label htmlFor="start-date">End Date</label>
+							<DatePicker
+								selected={endDate}
+								onChange={(date) => handleUpdateEndDate(date)}
+								dateFormat="dd MMMM yyyy"
+								minDate={startDate}
+								className="form-control"
+								id="start-date"
+							/>
+						</div>
+					</div>
+
+					<div className="form-group">
+						{days.map((day) => (
+							<div
+								className="form-check form-check-inline"
+								key={day.label}>
+								<input
+									className="form-check-input"
+									type="checkbox"
+									id={day.label}
+									name={day.label}
+									value={day.value}
+									onClick={props.handleUpdateDay}
+									disabled={!day.isAvailable}
+									onChange={handleCheckDay}
+									checked={day.checked && day.isAvailable}
+								/>
+								<label
+									className="form-check-label"
+									htmlFor="sunday">{day.label}</label>
+							</div>
+						))}
+					</div>
+				</div>
+				<button
+					className="btn btn-primary"
+					type="submit">Submit</button>
+			</form>
 		</>
 	);
 };
