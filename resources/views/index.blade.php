4eb5c88599a0ae4586c4161ee0be81b2b841edb7
pam <justin.parado.personal@gmail.com>
2020-12-17 09:41:48 +0800

    Set up of primary application page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>{{ env("APP_NAME") }}</title>
+
+		<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+	</head>
+	<body>
+		<script src="{{ asset('js/app.js') }}"></script>
+	</body>
+</html>
