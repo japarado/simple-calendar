20741f54cbc2412a7d9c8dbb4895930fea047012
pam <justin.parado.personal@gmail.com>
2020-12-17 11:22:03 +0800

    Initial calendar form

--- resources/js/pages/Home/Home.js
+++ resources/js/pages/Home/Home.js
@@ -7,10 +7,27 @@ const Home = () =>
 		<>
 			<Navbar/>
 
-			<main className="container-fluid">
+			<main className="container-fluid pt-5">
 				<div className="row">
 					<div className="col-md-4 col-xs-12">
-						Form
+
+						<div className="form-row">
+							<div className="form-group col-md-12">
+								<label htmlFor="event-name">Event Name</label>
+								<input
+									type="text"
+									className="form-control"
+									id="event-name"
+									name="event-name"
+									required/>
+							</div>
+						</div>
+
+						<div className="form-row">
+							<div className="form-group col-md-6">
+							</div>
+						</div>
+
 					</div>
 					<div className="col-md-8 col-xs-12">
 						Event list
