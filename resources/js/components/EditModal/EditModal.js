b02fa84eda8637768773fe8957e61679ddda7677
pam <justin.parado.personal@gmail.com>
2020-12-19 11:48:22 +0800

    Updating procedures

--- resources/js/components/EditModal/EditModal.js
+++ resources/js/components/EditModal/EditModal.js
@@ -8,13 +8,18 @@ import Form from "../Form/Form";
 import {eachDayOfInterval} from "date-fns";
 import {createDayArray, prettifyDate} from "../../utils";
 
+import {update} from "../../services/eventService";
+
 class EditModal extends Component
 {
 	state = {
+		eventId: undefined,
+		dateId: undefined,
 		name: "",
 		description: "",
 		startDate: new Date(),
 		endDate: new Date(),
+		eventDate: new Date(),
 		dates: [],
 		days: [],
 	}
@@ -22,10 +27,13 @@ class EditModal extends Component
 	componentDidMount()
 	{
 		this.setState({
+			eventId: this.props.eventId,
+			dateId: this.props.dateId,
 			name: this.props.name,
 			description: this.props.description,
 			startDate: this.props.startDate,
 			endDate: this.props.endDate,
+			eventDate: this.props.eventDate,
 			dates: this.props.dates,
 			days: createDayArray(),
 		}, () => this.setDayStates());
@@ -35,9 +43,6 @@ class EditModal extends Component
 	{
 		const selectedEventDays = Array.from(new Set(this.state.dates.map((date) => date.getDay())));
 		const availableEventDays = Array.from(new Set(eachDayOfInterval({start: this.state.startDate, end: this.state.endDate}).map((date) => date.getDay())));
-
-		console.log(selectedEventDays);
-		console.log(availableEventDays);
 
 		const days = createDayArray().map((dayObj) => 
 		{
@@ -65,36 +70,66 @@ class EditModal extends Component
 		this.setState({days});
 	}
 
-	adjustDays()
-	{
-		const eventDays = eachDayOfInterval({start: this.state.startDate, end: this.state.endDate}).map((day) => day.getDay());
-		const availableDays = this.state.days.map((day) => 
-		{
-			if(eventDays.includes(day.value))
-			{
-				day.isAvailable = day.checked = true;
-			}
-			else 
-			{
-				day.isAvailable = day.checked = false;
-			}
-			return day;
-		});
-		this.setState({days: availableDays});
-	}
+	// adjustDays()
+	// {
+	// 	const eventDays = eachDayOfInterval({start: this.state.startDate, end: this.state.endDate}).map((day) => day.getDay());
+	// 	const availableDays = this.state.days.map((day) => 
+	// 	{
+	// 		if(eventDays.includes(day.value))
+	// 		{
+	// 			day.isAvailable = day.checked = true;
+	// 		}
+	// 		else 
+	// 		{
+	// 			day.isAvailable = day.checked = false;
+	// 		}
+	// 		return day;
+	// 	});
+	// 	this.setState({days: availableDays});
+	// }
 
 	handleUpdateName = (name) => this.setState(({name}))
 
 	handleUpdateDescription = (description) => this.setState({description})
 
-	handleUpdateStartDate = (date) => 
-	{
-		this.setState({startDate: date}, () => this.setDayStates());
-	}
+	handleUpdateStartDate = (date) => this.setState({startDate: date}, () => this.setDayStates());
 
-	handleUpdateEndDate = (date) => 
+	handleUpdateEndDate = (date) => this.setState({endDate: date}, () => this.setDayStates());
+
+	handleCheckDay = (e) => 
 	{
-		this.setState({endDate: date}, () => this.setDayStates());
+		const updatedDays = this.state.days.map((day) => 
+		{
+			if(day.value === parseInt(e.target.value))
+			{
+				day.checked = e.target.checked;
+			}
+			return day;
+		});
+		this.setState({days: updatedDays});
+	};
+
+	handleSubmit = async (e) => 
+	{
+		e.preventDefault();
+		const selectedDays = this.state.days.filter((day) => day.checked).map((day) => day.value);
+		const event = {
+			name: this.state.name,
+			description: this.state.description,
+			dates: [],
+			startDate: this.state.startDate,
+			endDate: this.state.endDate
+		};
+
+		eachDayOfInterval({start: this.state.startDate, end: this.state.endDate}).forEach((date) => 
+		{
+			if(selectedDays.includes(date.getDay()))
+			{
+				event.dates.push(date);
+			}
+		});
+		await update(this.state.eventId, event);
+		await this.props.handleSubmit(e);
 	}
 
 	render()
@@ -109,10 +144,11 @@ class EditModal extends Component
 				<Modal.Header> 
 					<div className="d-flex flex-column">
 						<h1>{this.state.name}</h1>
-						<h5>{prettifyDate(this.props.eventDate)}</h5>
+						<h5>{prettifyDate(this.state.eventDate)}</h5>
 					</div>
 				</Modal.Header> 
 
+				{/* TODO: Unify all these props into a single object */}
 				<Modal.Body> 
 					<Form
 						name={this.state.name}
@@ -124,9 +160,8 @@ class EditModal extends Component
 						setDescription={this.handleUpdateDescription}
 						setStartDate={this.handleUpdateStartDate}
 						setEndDate={this.handleUpdateEndDate}
-						handleSubmit={this.props.handle}
-						handleCheckDay={() => 
-						{}}
+						handleSubmit={this.handleSubmit}
+						handleCheckDay={this.handleCheckDay}
 						submitText={"Save Changes"}
 					/>
 				</Modal.Body> 
